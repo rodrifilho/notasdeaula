b9f763238e656f42ebd6bbdd14c2e20ea55256b4
rodrifilho <145624436+rodrifilho@users.noreply.github.com>
2024-03-14 15:55:08 -0300

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Criando um professor
-        Professor professor = new Professor("Dr. Smith", 40, 5000.00);
-        // Criando um aluno
-        Aluno aluno = new Aluno("João", 20, "2023001");
+        Produto produto1 = new Produto("Camisa", 49.99, "Camisa polo azul");
+        Produto produto2 = new Produto("Calça", 99.99, "Calça jeans preta");
 
-        // Mostrando informações do professor
-        System.out.println("Informações do Professor:");
-        System.out.println("Nome: " + professor.getNome());
-        System.out.println("Idade: " + professor.getIdade());
-        System.out.println("Salário: R$" + professor.getSalario());
+        System.out.println("Informações do Produto 1:");
+        produto1.mostrarInformacoes();
 
-        // Mostrando informações do aluno
-        System.out.println("\nInformações do Aluno:");
-        System.out.println("Nome: " + aluno.getNome());
-        System.out.println("Idade: " + aluno.getIdade());
-        System.out.println("Matrícula: " + aluno.getMatricula());
+        System.out.println("\nInformações do Produto 2:");
+        produto2.mostrarInformacoes();
     }
 }
